b287a097fe8b94e7a1d2589aa4c3d698aae98f10
Nitin Verma <nitin.verma@remotestate.in>
2026-01-27 08:27:10 +0530

    wip

--- 25_student_api/cmd/students-api/main.go
+++ 25_student_api/cmd/students-api/main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/nitin0409sep/students-api/internal/config"
+)
+
+func main() {
+	// load config
+
+	cfg := config.MustLoad()
+
+	// database setup
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func (w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+
+	// setup server
+	server := http.Server {
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+
+	if(err != nil) {
+		log.Fatal("Failed to start server")
+	}
+
+}
